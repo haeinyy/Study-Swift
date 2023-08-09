67c9285732421715f481f9ced989afaacf29f483
haeinyy <godls4402@gmail.com>
2023-08-09 21:53:54 +0900

    Study Tab Controller

---
+++ swift_chapter10_tab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swift_chapter10_tab
+//
+//  Created by 해인 on 2023/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMovePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+    
+}
+

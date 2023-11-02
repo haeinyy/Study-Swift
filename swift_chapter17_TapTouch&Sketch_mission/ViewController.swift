4411b9c1b13d710da4a010176ed1d622ee18b9c8
haeinyy <godls4402@gmail.com>
2023-11-02 15:36:37 +0900

    Doit TapToduch&Sketch

---
+++ swift_chapter17_TapTouch&Sketch_mission/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  swift_chapter17_TapTouch&Sketch_mission
+//
+//  Created by 해인 on 2023/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var txtLineSize: UITextField!
+    
+    var lastPoint: CGPoint!
+    var lineSize:CGFloat = 2.0
+    var lineColor = UIColor.black.cgColor
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        lastPoint = touch.location(in: imgView)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
+        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
+        
+        let touch = touches.first! as UITouch
+        let currPoint = touch.location(in: imgView)
+        
+        imgView.image?.draw(in: CGRect(x: 0, y: 0, width: imgView.frame.size.width, height: imgView.frame.size.height))
+        
+        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: currPoint.x, y: currPoint.y))
+        UIGraphicsGetCurrentContext()?.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        lastPoint = currPoint
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
+        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
+        
+        imgView.image?.draw(in: CGRect(x: 0, y: 0, width: imgView.frame.size.width, height: imgView.frame.size.height))
+        
+        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            imgView.image = nil
+        }
+    }
+
+    @IBAction func txtTouchDown(_ sender: UITextField) {
+        txtLineSize.selectAll(UITextField.self)
+    }
+    
+    @IBAction func txtEditChange(_ sender: UITextField) {
+        if txtLineSize.text != "" {
+            lineSize =    CGFloat(Int(txtLineSize.text!)!)
+        }
+    }
+    
+    @IBAction func txtDisEndOnExit(_ sender: UITextField) {
+        lineSize =    CGFloat(Int(txtLineSize.text!)!)
+    }
+    
+    @IBAction func btnLineColorBlack(_ sender: UIButton) {
+        lineColor = UIColor.black.cgColor
+    }
+    
+    @IBAction func btnLineColorRed(_ sender: UIButton) {
+        lineColor = UIColor.red.cgColor
+    }
+    
+    @IBAction func btnLineColorGreen(_ sender: UIButton) {
+        lineColor = UIColor.green.cgColor
+    }
+    
+    @IBAction func btnLineColorBlue(_ sender: UIButton) {
+        lineColor = UIColor.blue.cgColor
+    }
+    
+    @IBAction func btnClear(_ sender: UIButton) {
+        imgView.image = nil
+    }
+}
+

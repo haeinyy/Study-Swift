b2da33da6d9b2fd1b822592edcfac1fa16b28406
haeinyy <godls4402@gmail.com>
2023-11-13 15:41:12 +0900

    Do it SwipeGesture mission

---
+++ swift_chapter18_SwipeGesture_mission/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  swift_chapter18_SwipeGesture_mission
+//
+//  Created by 해인 on 2023/11/13.
+//
+
+import UIKit
+
+var images = [ "01.png", "02.png", "03.png", "04.png", "05.png", "06.png" ]
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = UIColor.black
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        
+        imgView.image = UIImage(named: images[0])
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
+    }
+
+    @IBAction func pageChnage(_ sender: UIPageControl) {
+    }
+    
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            
+            // 오른쪽으로 밀면 이전 이미지
+            case UISwipeGestureRecognizer.Direction.right:
+                if (pageControl.currentPage>0) {
+                    pageControl.currentPage = pageControl.currentPage - 1
+                }
+                
+            // 왼쪽으로 밀면 다음 이미지
+            case UISwipeGestureRecognizer.Direction.left:
+                if (pageControl.currentPage<pageControl.numberOfPages-1) {
+                    pageControl.currentPage = pageControl.currentPage + 1
+                }
+
+            
+            default:
+                break
+            }
+            
+            imgView.image = UIImage(named: images[pageControl.currentPage])
+        }
+    }
+    
+}
+
